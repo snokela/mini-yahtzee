8a44d9dce9ac27e8dc7b55576a631fe2c1d92be9
saananokela <smnokela@gmail.com>
2024-09-25 16:05:58 +0300

    Added minimun content to the Gameboard component.

---
+++ components/Gameboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import styles from '../styles/Styles'
+
+export default function Gameboard() {
+  return (
+    <View>
+      <Text>
+        Gameboard will be here...
+      </Text>
+    </View>
+  )
+}
