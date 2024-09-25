4b08a70c043d637b8f37d8400fa760b6a8576849
saananokela <smnokela@gmail.com>
2024-09-25 16:08:12 +0300

    Added minimun content to the Scoreboard and Home -components.

---
+++ components/Scoreboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import styles from '../styles/Styles'
+
+export default function Scoreboard() {
+  return (
+    <View>
+      <Text>
+        Scoreboard will be here...
+      </Text>
+    </View>
+  )
+}
