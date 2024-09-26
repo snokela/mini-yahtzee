742ab477e6970b332191034b261cd68325f7bc1f
saananokela <smnokela@gmail.com>
2024-09-26 14:33:16 +0300

    Imported Header and Footer into Gameboard.js.

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -1,13 +1,19 @@
-import React from 'react'
 import { Text, View } from 'react-native'
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Header from './Header';
+import Footer from './Footer';
 import styles from '../styles/Styles'
 
 export default function Gameboard() {
   return (
+    <>
+    <Header />
     <View>
       <Text>
         Gameboard will be here...
       </Text>
     </View>
+    <Footer /> 
+    </>
   )
 }
