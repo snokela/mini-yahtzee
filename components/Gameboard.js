aa845370fdaf9d896e11ac0f6510cbc47f8bacb9
saananokela <smnokela@gmail.com>
2024-09-26 14:49:14 +0300

    Added dice rendering with Pressable component without functionality.

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Header from './Header';
 import Footer from './Footer';
@@ -6,7 +6,28 @@ import { NBR_OF_DICES, NBR_OF_THROWS, MIN_SPOT, MAX_SPOT, BONUS_POINTS_LIMIT, BO
 import { Container, Row, Col } from 'react-native-flex-grid';
 import styles from '../styles/Styles'
 
+let board = [];
+
 export default function Gameboard() {
+
+  const row = [];
+  for (let i = 0; i < NBR_OF_DICES; i++) {
+    row.push(
+      <Pressable
+          key={"row" + i}
+          // onPress={() => selectDice(i)}
+        >
+        <MaterialCommunityIcons
+          name={board[i]}
+          key={"row" + i}
+          size={50}
+          // color={getDiceColor(i)}
+        >
+        </MaterialCommunityIcons>
+      </Pressable>
+    );
+  }
+
   return (
     <>
     <Header />
