8b694efd178c497cb9633190dc5ab4c3934c94da
saananokela <smnokela@gmail.com>
2024-09-26 15:29:23 +0300

    Added state variables to manage the game on the Gameboard.

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Pressable, Text, View } from 'react-native'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Header from './Header';
@@ -9,6 +10,20 @@ import styles from '../styles/Styles'
 let board = [];
 
 export default function Gameboard() {
+
+  const [nbrOfThrowsLeft, setNbrOfThrowsLeft] = useState(NBR_OF_THROWS);    //three throws for each turn --> there are a total of 18 throws in one game
+  const [status, setStatus] = useState('Throw dices');
+  const [gameEndStatus, setGameEndStatus] = useState('false');
+
+  // if dices are selected or not
+  const [selectedDices, setSelectedDices] = useState(new Array(NBR_OF_DICES).fill(false));   // is dices selected or not in the game
+  // dice spots
+  const [diceSpots, setDiceSpots] =  useState( new Array(NBR_OF_DICES).fill(0));
+  // if dice points are selected on not for spots
+  const [selecteDicePoints, setSelectedDicePoints] = useState(new Array(MAX_SPOT).fill(false));
+  // total points for diffferent spots
+  const [DicePointsTotal, setDicePointsTotal] = useState(new Array(MAX_SPOT).fill(0));
+
 
   const row = [];
   for (let i = 0; i < NBR_OF_DICES; i++) {
