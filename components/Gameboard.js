22d82f7d1148356fa41b835851a1fb5a7ba01302
saananokela <smnokela@gmail.com>
2024-09-26 14:41:29 +0300

    Imported constants and container, row and col from react-native-flex-grid.

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -2,6 +2,8 @@ import { Text, View } from 'react-native'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Header from './Header';
 import Footer from './Footer';
+import { NBR_OF_DICES, NBR_OF_THROWS, MIN_SPOT, MAX_SPOT, BONUS_POINTS_LIMIT, BONUS_POINTS } from '../constants/Game';
+import { Container, Row, Col } from 'react-native-flex-grid';
 import styles from '../styles/Styles'
 
 export default function Gameboard() {
