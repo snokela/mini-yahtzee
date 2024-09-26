3c3b5b2c41a5d2ccbab64e335524bc4f904e70f1
saananokela <smnokela@gmail.com>
2024-09-26 10:37:02 +0300

    Added rules of the game and Play-button with basic styling.

--- components/Home.js
+++ components/Home.js
@@ -6,7 +6,7 @@ import Footer from './Footer';
 import { NBR_OF_DICES, NBR_OF_THROWS, MIN_SPOT, MAX_SPOT, BONUS_POINTS_LIMIT, BONUS_POINTS } from '../constants/Game';
 import styles from '../styles/Styles';
 
-export default function Home() {
+export default function Home({ navigation }) {
 
   const [playerName, setPlayerName] = useState('');
   const [hasPlayerName, setHasPlayerName] = useState(false);
@@ -37,6 +37,25 @@ export default function Home() {
           onPress={() => handlePlayerName(playerName)}
         >
           <Text style={styles.buttonText}>OK</Text>
+        </Pressable>
+        <Text>Rules of the game</Text>
+        <Text multiline='true'>
+          THE GAME: Upper section of the classic Yahtzee
+          dice game. You have {NBR_OF_DICES} dices and
+          for the every dice you have {NBR_OF_THROWS}
+          throws. After each throw you can keep dices in
+          order to get same dice spot counts as many as
+          possible. In the end of the turn you must select
+          your points from {MIN_SPOT} to {MAX_SPOT}.
+          Game ends when all points have been selected.
+          The order for selecting those is free.
+        </Text>
+        <Text>Good luck, {playerName}!</Text>
+        <Pressable
+          style={styles.button}
+          onPress={() => navigation.navigate('Gameboard')}
+        >
+          <Text style={styles.buttonText}>PLAY</Text>
         </Pressable>
       </View>
       <Footer />
