3690b698bc1543b1874e18a7da08a69fac8c6adc
saananokela <smnokela@gmail.com>
2024-09-26 10:55:33 +0300

    Added conditional rendering for player name and game rules.

--- components/Home.js
+++ components/Home.js
@@ -27,36 +27,55 @@ export default function Home({ navigation }) {
           size={90}
           color='#D87093'
         />
-        <Text>For scoreboard enter your name...</Text>
-        <TextInput
-          onChangeText={setPlayerName}
-          autoFocus={true}
-        />
-        <Pressable
-          style={styles.button}
-          onPress={() => handlePlayerName(playerName)}
-        >
-          <Text style={styles.buttonText}>OK</Text>
-        </Pressable>
-        <Text>Rules of the game</Text>
-        <Text multiline='true'>
-          THE GAME: Upper section of the classic Yahtzee
-          dice game. You have {NBR_OF_DICES} dices and
-          for the every dice you have {NBR_OF_THROWS}
-          throws. After each throw you can keep dices in
-          order to get same dice spot counts as many as
-          possible. In the end of the turn you must select
-          your points from {MIN_SPOT} to {MAX_SPOT}.
-          Game ends when all points have been selected.
-          The order for selecting those is free.
-        </Text>
-        <Text>Good luck, {playerName}!</Text>
-        <Pressable
-          style={styles.button}
-          onPress={() => navigation.navigate('Gameboard')}
-        >
-          <Text style={styles.buttonText}>PLAY</Text>
-        </Pressable>
+        {!hasPlayerName ? (
+          <>
+            <Text>For scoreboard enter your name...</Text>
+            <TextInput
+              onChangeText={setPlayerName}
+              autoFocus={true}
+            />
+            <Pressable
+              style={styles.button}
+              onPress={() => handlePlayerName(playerName)}
+            >
+              <Text style={styles.buttonText}>OK</Text>
+            </Pressable>
+          </>
+        ) : (
+          <>
+            <Text>Rules of the game</Text>
+            <Text multiline='true'>
+              THE GAME: Upper section of the classic Yahtzee
+              dice game. You have {NBR_OF_DICES} dices and
+              for the every dice you have {NBR_OF_THROWS}
+              throws. After each throw you can keep dices in
+              order to get same dice spot counts as many as
+              possible. In the end of the turn you must select
+              your points from {MIN_SPOT} to {MAX_SPOT}.
+              Game ends when all points have been selected.
+              The order for selecting those is free.
+
+              POINTS: After each turn game calculates the sum
+              for the dices you selected. Only the dices having
+              the same spot count are calculated. Inside the
+              game you can not select same points from
+              {MIN_SPOT} to {MAX_SPOT} again.
+
+              GOAL: To get points as much as possible.
+              {BONUS_POINTS_LIMIT} points is the limit of
+              getting bonus which gives you {BONUS_POINTS}
+              points more.
+            </Text>
+            <Text>Good luck, {playerName}!</Text>
+            <Pressable
+              style={styles.button}
+              onPress={() => navigation.navigate('Gameboard')}
+            >
+              <Text style={styles.buttonText}>PLAY</Text>
+            </Pressable>
+          </>
+        )
+        }
       </View>
       <Footer />
     </>
