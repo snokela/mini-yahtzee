c7f8ede69ace8253ca743cd3c071a3b51c141254
saananokela <smnokela@gmail.com>
2024-09-26 09:18:12 +0300

    Added semicolons to imports in Footer.js and added imports to Home.js.

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import styles from '../styles/Styles'
+import { useState } from 'react';
+import { Text, View, TextInput, Pressable, Keyboard } from 'react-native';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Header from './Header';
+import Footer from './Footer';
+import { NBR_OF_DICES, NBR_OF_THROWS, MIN_SPOT, MAX_SPOT, BONUS_POINTS_LIMIT, BONUS_POINTS } from '../constants/Game';
+import Styles from '../styles/Styles';
 
 export default function Scoreboard() {
   return (
