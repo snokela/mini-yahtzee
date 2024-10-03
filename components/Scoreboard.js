dc1687ff2e42475136b0761bcb889fe74b0b0740
saananokela <smnokela@gmail.com>
2024-10-03 13:10:09 +0300

    Added basic structure for scoreBoard with hardcoded values.

--- components/Scoreboard.js
+++ components/Scoreboard.js
@@ -3,15 +3,36 @@ import styles from '../styles/Styles'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Header from './Header';
 import Footer from './Footer';
+import { DataTable } from 'react-native-paper';
+
+
+
+const scores = [
+  { id: 1, name: 'kalle', date: '6.12.2023', time: '12.15', points: 80 },
+  { id: 2, name: 'matti', date: '6.12.2023', time: '12.15', points: 75 },
+  { id: 3, name: 'heli', date: '6.12.2023', time: '12.15', points: 55 },
+  { id: 4, name: 'ritva', date: '6.12.2023', time: '12.15', points: 40 },
+]
 
 export default function Scoreboard() {
   return (
     <>
-    <Header />
+      <Header />
       <View>
-        <Text>
-          Scoreboard will be here...
-        </Text>
+        <Text>Top Six</Text>
+        <DataTable>
+          {scores.slice().map((item) => (
+            <DataTable.Row key={item.id}>
+              <DataTable.Cell>{item.id}</DataTable.Cell>
+              <DataTable.Cell>{item.name}</DataTable.Cell>
+              <DataTable.Cell numeric>{item.date}</DataTable.Cell>
+              <DataTable.Cell numeric>{item.time}</DataTable.Cell>
+              <DataTable.Cell numeric>{item.points}</DataTable.Cell>
+            </DataTable.Row>
+          ))}
+          <DataTable.Row>
+          </DataTable.Row>
+        </DataTable>
       </View>
       <Footer />
     </>
