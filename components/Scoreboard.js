f57f461548e57e5a7f6b83bdee11bb0380386eed
saananokela <smnokela@gmail.com>
2024-10-04 11:47:23 +0300

    Imported Scoreboard_key into scoreboard.js and added basic getData functio.

--- components/Scoreboard.js
+++ components/Scoreboard.js
@@ -4,6 +4,7 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Header from './Header';
 import Footer from './Footer';
 import { DataTable } from 'react-native-paper';
+import { SCOREBOARD_KEY,  MAX_NBR_OF_SCOREBOARD_ROWS} from '../constants/Game';
 
 const scores = [
   // { name: 'Kalle', date: '6.12.2023', time: '12.15', points: 120 },
@@ -17,6 +18,16 @@ export default function Scoreboard() {
 
   // LISÄÄ TÄHÄN useFOCUSEffec esim renderöimään aina kun screeni avataan uudelleen!
   // https://reactnavigation.org/docs/function-after-focusing-screen/
+
+    // get earlier data from asyncstorage
+    const getData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem(SCOREBOARD_KEY);
+        return jsonValue != null ? JSON.parse(jsonValue) : null;
+      } catch (e) {
+        console.log(e);
+      }
+    };
 
   // For sorting scoreboard data according to number of points you can use sort()
 // function: HUOM! MUISTA UUSI taulukko const järjestetty = [...scores]
