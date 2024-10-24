a89c0c746d2eafef0977228ac523d2330e0996db
saananokela <smnokela@gmail.com>
2024-10-24 18:10:29 +0300

    Cleaned up styles.js code.

--- styles/Styles.js
+++ styles/Styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   header: {
@@ -28,27 +28,20 @@ export default StyleSheet.create({
     margin: 10,
   },
   diceRow: {
-    // marginBottom: 20,
-    // marginVertical: 15,
     paddingHorizontal: 20,
-    // paddingHorizontal: 35,
     justifyContent: 'space-between',
   },
   pointsRow: {
     paddingHorizontal: 20,
-    // paddingHorizontal: 35,
     justifyContent: 'space-between',
-    // marginLeft: 25,
     marginTop: 20,
   },
   pointsToSelectRow: {
     paddingHorizontal: 20,
-    // paddingHorizontal: 35,
     justifyContent: 'space-between',
     marginBottom: 20,
   },
   iconCol: {
-    // marginHorizontal: -10,
     alignItems: 'center',
   },
   button: {
@@ -95,7 +88,6 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // height: Dimensions.get('screen').height,
   },
   labelText: {
     marginTop: 15,
@@ -117,14 +109,12 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // height: Dimensions.get('screen').height,
   },
   scoreboardContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     margin: 20,
-    // height: Dimensions.get('screen').height,
   },
   boldText: {
     fontWeight: 'bold',
